981c3059eec0a2d7d3b78bd53dac1fab3fba6938
somtonu <onusomtochukwusamuel@gmail.com>
2022-06-30 07:08:31 -0700

    print output to screen

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ * Return: is always 0
+*/
+
+int main(void)
+i
+{
+	char alx[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", alx);
+	return (0);
+}

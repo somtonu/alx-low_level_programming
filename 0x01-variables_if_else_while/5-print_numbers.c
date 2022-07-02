5816c399dc82b9e9855cbba1c54eef57e26f3b56
somtonu <onusomtochukwusamuel@gmail.com>
2022-07-01 20:50:31 -0700

    timeout

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int a;
+
+	a = 0;
+
+	while (a < 10)
+{
+	printf("%d", a);
+	a++;
+}
+
+{
+	putchar('\n');
+}
+
+	return (0);
+}

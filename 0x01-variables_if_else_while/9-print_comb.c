bdf65221c20cf37dd533d91082db72ad97f8ff81
somtonu <onusomtochukwusamuel@gmail.com>
2022-07-01 21:59:51 -0700

    combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+* main - program prints numbers ten
+*
+* Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+{
+	putchar(n + '0');
+
+	if (n < 9)
+{
+	putchar(',');
+	putchar(' ');
+}
+}
+
+{
+	putchar('\n');
+}
+
+	return (0);
+}

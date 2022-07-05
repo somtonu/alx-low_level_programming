be0376b571a17c7c302a3ff279dcf0178ccd6d05
somtonu <onusomtochukwusamuel@gmail.com>
2022-07-05 23:00:57 +0100

    finally get it

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,25 +1,13 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
  * main - prints the alphabets in lowercase
  *
  * Return: 0
 */
-
 int main(void)
-{
-	int letter;
-
-	for (letter = 97 ; letter <= 122 ; letter++)
 
 {
-	putchar(letter);
-}
-
-{
-	putchar('\n');
-}
+	print_alphabet();
 
 	return (0);
 }

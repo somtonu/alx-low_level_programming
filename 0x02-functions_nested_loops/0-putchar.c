32cec09076728016cbf4c0b7a29ee844ae288921
somtonu <onusomtochukwusamuel@gmail.com>
2022-07-05 19:23:12 +0100

    Test1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - a program that prints _putchar
+ *
+ * Return: 0
+ *
+*/
+int main(void)
+{
+	char p[] = "_putchar";
+
+	int i;
+
+	for (i = 0 ; i <= 8 ; i++)
+{
+	putchar(p[i]);
+}
+
+{
+	putchar('\n');
+}
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This readme contains the various codes in c for the nested loops task in the alx software engineering programme. (https://twitter.com/darthzanobi)

06b8dc4553f0143a93b26b8af59b934ca4804490
somtonu <onusomtochukwusamuel@gmail.com>
2022-07-05 21:55:00 +0100

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - prints the alphabets in lowercase
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int letter;
+
+	for (letter = 97 ; letter <= 122 ; letter++)
+
+{
+	putchar(letter);
+}
+
+{
+	putchar('\n');
+}
+
+	return (0);
+}

5acb621e90136f678b9d0f37c0722fe32eaee303
somtonu <onusomtochukwusamuel@gmail.com>
2022-07-06 16:06:10 +0100

    |absolute value|

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - absolute value of an integer
+ * @v: is an integer
+ *
+ * Return: 0 on success
+*/
+int _abs(int v)
+{
+	if (v < 0)
+	return (-v);
+
+	else
+	return (v);
+}

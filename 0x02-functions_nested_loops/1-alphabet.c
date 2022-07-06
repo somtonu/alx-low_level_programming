9fba819de4ed44d4baeae8797cf6bae3f75b2daf
somtonu <onusomtochukwusamuel@gmail.com>
2022-07-06 14:04:18 +0100

    new era

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 /**
  * print_alphabet - a function that prints the alphabet in lowercase
  *
- * Return: Always 0 (Success)
+ * Return: nothing
 */
 void print_alphabet(void)
 {
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,17 +6,17 @@
 */
 void print_alphabet_x10(void)
 {
-	int n, co;
+	int n, m;
 
-	co = 0;
+	m = 0;
 
-	while (co < 10)
+	while (m < 10)
 {
 	for (n = 'a'; n <= 'z'; n++)
 {
 	_putchar(n);
 }
-	co++;
+	m++;
 	_putchar('\n');
 }
 }

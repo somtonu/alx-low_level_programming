6a3c893ab73f8b631358c222af97892745f4cecf
somtonu <onusomtochukwusamuel@gmail.com>
2022-07-07 21:07:18 +0100

    omit 2 & 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints "0123456789\n"
+ *
+ * Return: 0
+*/
+void print_most_numbers(void)
+{
+	int i = 47;
+
+	while (i <= 56)
+{
+	i++;
+
+	if (i != 50 && i != 52)
+{
+
+	_putchar(i);
+}
+}
+
+{
+	_putchar('\n');
+}
+}

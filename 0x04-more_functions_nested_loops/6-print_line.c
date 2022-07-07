a53eb19091fc6cf7cd3a3f7bd9dd58b55f49d208
somtonu <onusomtochukwusamuel@gmail.com>
2022-07-07 22:22:23 +0100

    underscore

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line
+ * @n: a character
+ * Return: 0
+*/
+void print_line(int n)
+{
+	int u;
+
+	for (u = 0; u < n; u++)
+{
+	_putchar('_');
+}
+{
+	_putchar('\n');
+}
+}	

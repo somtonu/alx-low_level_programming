4a34890688a57c797af6f29e5806ca0483455dca
somtonu <onusomtochukwusamuel@gmail.com>
2022-07-07 12:11:09 +0100

    test run

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* positive_or_negative - function that prints if int is positive or negative
+* @i: is an integer
+* Return: 0
+*/
+
+void positive_or_negative(int i)
+{
+	i = 98;
+
+	positive_or_negative(i);
+
+
+}

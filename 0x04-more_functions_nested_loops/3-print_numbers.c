7765f1a27291cc843e79aca096e56df3bff71503
somtonu <onusomtochukwusamuel@gmail.com>
2022-07-07 20:43:30 +0100

    getting a hang of it

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_numbers - prints "0123456789\n"
+ *
+ * Return: 0
+*/
+void print_numbers(void)
+{
+	int i = 47;
+
+	while (i <= 56)
+{
+	i++;
+
+	_putchar(i);
+}
+
+{
+	_putchar('\n');
+}
+}

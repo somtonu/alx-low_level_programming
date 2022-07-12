fab02537477c73fddbf47effb90541f30f24d1b9
somtonu <onusomtochukwusamuel@gmail.com>
2022-07-12 19:39:41 +0100

    header file

---
+++ 0x05-pointers_array_strings/main.h
@@ -0,0 +1,45 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+void pr_digit(int charcode, int fmt);
+int add(int a, int b);
+void pr_with_fmt(int charcode, int fmt);
+void print_to_98(int n);
+void pr_3_digit(int charcode, int fmt);
+void print_times_table(int n);
+void print_spaces(int n);
+void print_zero(void);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+void rev_string(char *s);
+
+#endif

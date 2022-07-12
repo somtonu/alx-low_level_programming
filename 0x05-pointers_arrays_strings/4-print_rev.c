73e66ab685e2cdef12339d68c126a4006d07e703
somtonu <onusomtochukwusamuel@gmail.com>
2022-07-12 21:22:49 +0100

    reversed

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - print %s in rev
+ * @s: string
+ *
+ * Return: 0
+*/
+void print_rev(char *s)
+{
+	int r = 0;
+
+	for (r = 0; s[r] != '\0'; r++)
+{
+
+}
+
+	for (r = r - 1; r >= 0; r--)
+{
+	_putchar(s[r]);
+}
+	_putchar('\n');
+}

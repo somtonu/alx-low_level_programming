26ef8c0a3cf95fb0885b2e6be0a1b36b5ff7cad3
somtonu <onusomtochukwusamuel@gmail.com>
2022-07-13 00:10:02 +0100

    cpy src to dest

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strcpy - copies string to dest
+ * @dest: destination
+ * @src: source
+ *
+ * Return: 0
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+
+	while (*(src + i) != '\0')
+{
+	i++;
+}
+	for (j = 0; j < i; j++)
+{
+	dest[j] = src[j];
+}
+	dest[i] = '\0';
+	return (dest);
+}

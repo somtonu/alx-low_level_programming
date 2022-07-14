c2211734ba06a1cb05e892e12110e08cc59e33ff
somtonu <onusomtochukwusamuel@gmail.com>
2022-07-14 22:37:10 +0100

    main.h update| first task push

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -41,5 +41,20 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 void rev_string(char *s);
+char *_strcat(char *dest, char *src);
+void print_number(int n);
+int _putchar(char c);
+void print_buffer(char *b, int size);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+
 
 #endif

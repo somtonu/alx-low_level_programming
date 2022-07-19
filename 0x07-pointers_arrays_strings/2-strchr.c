2cd517a8bc2772647d1ce9391d03903eceb28e64
somtonu <onusomtochukwusamuel@gmail.com>
2022-07-20 00:23:01 +0100

    char detect

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strchr - locates a char in a str
+ * @s: string
+ * @c: char
+ * Return: * to char, 0 if char absent
+ */
+char *_strchr(char *s, char c)
+{
+	int a;
+
+	for (i = 0; s[i] != '\0'; ++i)
+{
+	if (s[i] == c)
+	return (&s[i]);
+}
+}

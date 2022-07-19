aa8a7f2b6622aba4150af2bffe3f615b78952bee
somtonu <onusomtochukwusamuel@gmail.com>
2022-07-20 00:10:40 +0100

    memory address copy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memcpy - copies memory area
+ * @dest: destination
+ * @src: source to be copied
+ * @n: no. of copies
+ * Return: *dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int a = 0;
+	int b = 0;
+
+	for (n > 0; dest[i] = src[j]; n--)
+{
+	i++;
+	j++;
+}
+	return (dest);
+}

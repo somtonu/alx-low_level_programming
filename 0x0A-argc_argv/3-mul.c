db0c9941aebc69a371204aa6b25a3763341308ad
somtonu <onusomtochukwusamuel@gmail.com>
2022-07-23 19:51:10 +0100

    multiply arr[1] arr[2]

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,16 +9,15 @@
  */
 int main(int argc, char **argv)
 {
-	if (argc != 3)
-{
-	printf("Error\n");
-
-	return (1);
-}
-	else if (argc == 3)
+	if (argc == 3)
 {
 	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 
 	return (0);
+}
+	else
+{
+	printf("Error\n");
+	return (1);
 }
 }

47716dfeb064526651554fc3b7cf12e78af88af4
somtonu <onusomtochukwusamuel@gmail.com>
2022-07-23 18:37:29 +0100

    update README.md and main.h

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - function
+ * @argc: arg count
+ * @argv: argument vector
+ * Return: int
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", *argv);
+	return (0);
+}
+
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+This repository contains solution to the alx task argc, argv.
+(https://twitter.com/darthzanobi)
+

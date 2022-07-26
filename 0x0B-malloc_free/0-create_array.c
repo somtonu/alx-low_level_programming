7f4e24bfb3317dc0bd21e7882a44478eb22ae608
somtonu <onusomtochukwusamuel@gmail.com>
2022-07-26 23:43:48 +0100

    create char[array]

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,7 +9,7 @@
 char *create_array(unsigned int size, char c)
 {
 	char *p;
-	unsigned int i = 0;
+	unsigned int i;
 
 	if (size == 0)
 	return (NULL);
@@ -19,7 +19,7 @@ char *create_array(unsigned int size, char c)
 	if (p == NULL)
 	return (NULL);
 
-	while (i < size)
+	while (i = 0 && i < size)
 {
 	i++;
 	p[i] = c;

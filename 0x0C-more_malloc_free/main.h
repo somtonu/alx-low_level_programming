e0207532b2851e7f2ae470bcb434878be96956c7
somtonu <onusomtochukwusamuel@gmail.com>
2022-07-28 22:32:53 +0100

    comment out line 72 on main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -69,7 +69,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-int main(int argc, char **argv);
+/* int main(int argc, char **argv); */
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);

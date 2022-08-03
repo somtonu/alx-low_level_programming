be7e2321f545924fcb462adedeb2bddcaa2a17e4
somtonu <onusomtochukwusamuel@gmail.com>
2022-08-03 05:59:53 +0100

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints name of file compiled then new line
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __BASE_FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+Contains solutions to the preprocessor tasks.

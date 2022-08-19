d9ac08d6f31d3a286a67b027b14e540c0d4497e8
somtonu <onusomtochukwusamuel@gmail.com>
2022-08-19 06:00:46 +0100

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,23 +1,62 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to unsigned int
- * @b: string containing the binary number
- * Return: the converted number
+  * _strlen - function to get the length of a string
+  * @c: pointer to char
+  *
+  * Return: the length of the string
+  */
+int _strlen(const char *c)
+{
+	int i = 0;
+
+	while (c[i++])
+		;
+	return (i - 1);
+}
+
+/**
+ * _pow - function to get the exponent of a number
+ * @num: number to be exponentiated
+ * @power: power to raise the number to
+ *
+ * Return: an unsigned int
+ */
+unsigned int _pow(unsigned int num, int power)
+{
+	unsigned int result = 1;
+
+	while (power >= 1)
+	{
+		result *= num;
+		power--;
+	}
+	return (result);
+}
+
+/**
+ * binary_to_uint - function to convert a binary number to an unsigned int
+ * @b: pointer to char holding the binary string
+ *
+ * Return: the converted number or 0 if
+ * b is null or string contains any character other than 0 and 1
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int dec_val = 0;
+	int len, power, i = 0;
+	unsigned int result = 0;
 
-	if (!b)
+	if (b == NULL)
 		return (0);
-
-	for (i = 0; b[i]; i++)
+	len = _strlen(b);
+	power = len - 1;
+	while (b[i] != '\0')
 	{
-		if (b[i] < '0' || b[i] > '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
+		result += (b[i] - '0') * _pow(2, power);
+		i++;
+		power--;
 	}
-	return (dec_val);
+	return (result);
 }

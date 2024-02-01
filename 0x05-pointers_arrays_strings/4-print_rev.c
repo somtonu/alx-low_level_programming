e9f01d7e25b2bdb9461ec59a6aae72b16119143a
Somtochukwu Onu <106788533+somtonu@users.noreply.github.com>
2024-02-01 19:40:03 +0100

    test new code

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,11 +1,11 @@
-#include "main.h"
+/* #include "main.h"
 /**
  * print_rev - print %s in rev
  * @s: string
  *
  * Return: 0
 */
-void print_rev(char *s)
+/* void print_rev(char *s)
 {
 	int r = 0;
 
@@ -18,4 +18,23 @@ void print_rev(char *s)
 	_putchar(s[r]);
 }
 	_putchar('\n');
+} */
+#include "main.h"
+#include <stdio.h>
+
+void print_rev(char *s)
+{
+	int len = 0;
+	int r;
+	int b;
+	for (r = 0; s[r] != '\0'; r++)
+{
+	len++;
+}
+	for (b = len; s[b] >= s[len]; b--)
+{
+	putchar(s[b]);
+}
+	putchar('\n');
+	/*    printf("lenght 63? = %d\n", len); */
 }
